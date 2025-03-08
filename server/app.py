e7d6c733fea75238d35f7bccd587dc7cb68ca184
Paul <paul.kyalo@student.moringaschool.com>
2025-03-08 17:24:00 +0300

    Commit for python-p4-chatterbox-lab

--- server/app.py
+++ server/app.py
@@ -1,26 +1,52 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 CORS(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
+@app.route('/')
+def home():
+    return "Welcome to the Chatterbox API!"
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET', 'POST'])
+def handle_messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def handle_message(id):
+    message = Message.query.get_or_404(id)
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        if 'body' in data:
+            message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000, debug=True)
